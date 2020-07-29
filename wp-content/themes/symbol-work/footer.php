85bf4e6355a1018e7cee95b81c4aba56c4d90b08
andrew <andrew.dickens@manifesto.co.uk>
2020-07-29 17:26:04 +0100

    header and logo

--- wp-content/themes/symbol-work/footer.php
+++ wp-content/themes/symbol-work/footer.php
@@ -1,7 +1,16 @@
 <div class="push-down"></div>
-<footer>
-    <p>footer</p>
-</footer>
+
+<div class="footer-wrapper">
+  <footer class="footer">
+
+    <div class="footer-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/nav-logo.png" srcset="<?php echo get_template_directory_uri();?>/images/nav-logo@2x.png 2x,
+             <?php echo get_template_directory_uri(); ?>/images/nav-logo@3x.png 3x" class="Symbolwork_Web">
+    </div>
+
+  </footer>
+</div>
+
 
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/symbol-work/inc/nav.php
+++ wp-content/themes/symbol-work/inc/nav.php
@@ -1,11 +1,13 @@
 <div class="nav-wrapper">
 
-    <nav class="nav">
-        <div class="nav__logo">
-            <h2>LOGO</h2>
-        </div>
+  <nav class="nav">
+    <div class="nav__logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/nav-logo.png" srcset="<?php echo get_template_directory_uri();?>/images/nav-logo@2x.png 2x,
+             <?php echo get_template_directory_uri(); ?>/images/nav-logo@3x.png 3x" class="Symbolwork_Web">
 
-        <?php 
+    </div>
+
+    <?php 
         wp_nav_menu( array(
             'menu' => 'navigation', 
             'menu_class' => 'nav__menu',
@@ -13,5 +15,5 @@
         ) );
    ?>
 
-    </nav>
+  </nav>
 </div>
